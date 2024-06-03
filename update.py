6a0218cc4a9a5d2edff2502558256b7593f4894e
F1owKang <27006147+f1owkang@users.noreply.github.com>
2024-06-03 17:39:57 +0800

    fix: import-im6.q16: unable to open X server `' @ error/import.c/ImportImageCommand/346.

---
+++ update.py
@@ -0,0 +1,8 @@
+import yaml
+
+with open('feeds.yml', 'r') as f:
+    data = yaml.safe_load(f)
+    repos = data.get('repositories', [])
+    for repo in repos:
+        print(f"::set-output name={repo['name']}_url::{repo['url']}")
+        print(f"::set-output name={repo['name']}_path::{repo['path']}")
