183677b3d6b514c305e68a056a3ea85bb31394c7
F1owKang <27006147+f1owkang@users.noreply.github.com>
2024-06-03 17:56:09 +0800

    fix: update py

--- update.py
+++ update.py
@@ -1,11 +1,18 @@
 import yaml
 import os
+
 cwd = os.getcwd()
 print("Current working directory:", cwd)
 
 with open('./feeds.yaml', 'r') as f:
     data = yaml.safe_load(f)
     repos = data.get('repositories', [])
+    repo_names = []
     for repo in repos:
-        print(f"::set-output name={repo['name']}_url::{repo['url']}")
-        print(f"::set-output name={repo['name']}_path::{repo['path']}")
+        name = repo['name']
+        repo_names.append(name)
+        print(f"::set-output name={name}_url::{repo['url']}")
+        print(f"::set-output name={name}_path::{repo['path']}")
+    
+    # 输出所有仓库名称，以空格分隔
+    print(f"::set-output name=repo_names::{' '.join(repo_names)}")
