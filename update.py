ea655598f25a33864018ccca0e4ebdf71086a6ea
F1owKang <27006147+f1owkang@users.noreply.github.com>
2024-06-03 18:00:29 +0800

    fix: outputs

--- update.py
+++ update.py
@@ -1,4 +1,5 @@
 import yaml
+import json
 import os
 
 cwd = os.getcwd()
@@ -7,12 +8,16 @@ print("Current working directory:", cwd)
 with open('./feeds.yaml', 'r') as f:
     data = yaml.safe_load(f)
     repos = data.get('repositories', [])
-    repo_names = []
+    repos_output = []
+
     for repo in repos:
-        name = repo['name']
-        repo_names.append(name)
-        print(f"::set-output name={name}_url::{repo['url']}")
-        print(f"::set-output name={name}_path::{repo['path']}")
-    
-    # 输出所有仓库名称，以空格分隔
-    print(f"::set-output name=repo_names::{' '.join(repo_names)}")
+        repos_output.append({
+            "name": repo['name'],
+            "url": repo['url'],
+            "path": repo['path']
+        })
+
+    # 将仓库信息转换为 JSON 字符串并写入文件
+    with open('repos.json', 'w') as json_file:
+        json.dump(repos_output, json_file)
+    print("Repositories information has been written to repos.json")
